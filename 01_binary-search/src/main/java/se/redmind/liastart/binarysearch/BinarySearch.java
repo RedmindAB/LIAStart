07f78480e76e5436368789df328ff930f56539b7
Julia Gu <Juejing.Gu@yh.nackademin.se>
2019-09-09 19:47:32 +0200

    julia first commit

--- 01_binary-search/src/main/java/se/redmind/liastart/binarysearch/BinarySearch.java
+++ 01_binary-search/src/main/java/se/redmind/liastart/binarysearch/BinarySearch.java
@@ -1,5 +1,7 @@
 package se.redmind.liastart.binarysearch;
 
+import java.util.Arrays;
+
 public interface BinarySearch {
 
 	int chop(int key, int[] values);
--- 01_binary-search/src/test/java/se/redmind/liastart/binarysearch/BinarySearchTest.java
+++ 01_binary-search/src/test/java/se/redmind/liastart/binarysearch/BinarySearchTest.java
@@ -1,19 +1,27 @@
 package se.redmind.liastart.binarysearch;
 
+import java.util.Arrays;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class BinarySearchTest {
-	
-	private BinarySearch search = new BinarySearch() {
-		public int chop(int key, int[] values) {
-			return -1;
-		}
-	};
 
-	@Test
-	public void test() {
+    private BinarySearch search = new BinarySearch() {
+        public int chop(int key, int[] values) {
+
+            Arrays.sort(values);
+            int result = Arrays.binarySearch(values, key);
+            if (result <= -1) {
+                return -1;
+            } else {
+                return result;
+            }
+        }
+    };
+
+    @Test
+    public void test() {
 		assertEquals(-1, chop(3, new int[] {}));
 		assertEquals(-1, chop(3, new int[] { 1 }));
 		assertEquals(0, chop(1, new int[] { 1 }));
@@ -35,10 +43,10 @@ public class BinarySearchTest {
 		assertEquals(-1, chop(4, new int[] { 1, 3, 5, 7 }));
 		assertEquals(-1, chop(6, new int[] { 1, 3, 5, 7 }));
 		assertEquals(-1, chop(8, new int[] { 1, 3, 5, 7 }));
-	}
+    }
 
-	private int chop(int key, int[] values) {
-		return search.chop(key, values);
-	}
+    private int chop(int key, int[] values) {
+        return search.chop(key, values);
+    }
 
 }
