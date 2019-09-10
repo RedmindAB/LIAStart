195e4daab51285831bc375c77d878773ef385131
Gustav Holfve <gustav.holfve@redmind.se>
2019-09-10 15:26:04 +0200

    Fixed conways folder struct

---
+++ 02_conways-game-of-life/src/main/java/se/redmind/liastart/Main.java
@@ -0,0 +1,9 @@
+package se.redmind.liastart;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO: Implement Conways Game of Life
+	}
+
+}
---
+++ 02_conways-game-of-life/src/test/java/se/redmind/liastart/MainTest.java
@@ -0,0 +1,14 @@
+package se.redmind.liastart;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void test() {
+		fail("This test fails");
+	}
+	
+}
